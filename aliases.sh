04f4f464f8133d524a849b39ca9a6b861fdbba45
Christhopher Lion <christhopherleao@icloud.com>
2018-10-29 23:01:12 -0300

    first commit

---
+++ aliases.sh
@@ -0,0 +1,10 @@
+alias code="code-insiders"
+alias alia="ssh -R 52698:localhost:52698 alia.ml"
+
+alias stop="docker-compose kill"
+alias restart="docker-compose restart"
+alias containers="ctop -s mem"
+alias log="docker-compose logs -f --tail 4"
+alias logtail="docker-compose logs -f --tail 50"
+alias build="docker-compose build --no-cache --pull"
+alias up="docker-compose up -d"
---
+++ sync.sh
@@ -0,0 +1,4 @@
+download-config() {
+  # ZSHRC
+  curl -L https://gist.github.com/itsmelion/79c6c87287160bdb161ebfa856243693 -o $HOME/.zshrc
+}
