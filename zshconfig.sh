a7d12e34f54d46c7b4291d4a8555abe556ba2cd3
Chris Lion <christhopherleao@icloud.com>
2018-12-15 19:14:17 -0200

    added zsh required stuff

--- zshconfig.sh
+++ zshconfig.sh
@@ -1,6 +1,11 @@
-# If you come from bash you might have to change your $PATH.
-export PATH=$HOME/bin:/usr/local/bin:$PATH
+if [[ "`uname`" == "Darwin"* ]]; then
+  # If you come from bash you might have to change your $PATH.
+  export PATH=$HOME/bin:/usr/local/bin:$PATH
+fi
 
+# Path to your oh-my-zsh installation.
+export ZSH=$HOME/.oh-my-zsh
+source $ZSH/oh-my-zsh.sh
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
