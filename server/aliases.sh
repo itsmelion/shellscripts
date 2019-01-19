a3ded821f3e74b513f73272c707882e6f8af4aa2
christhopher.leao <christhopherleao@icloud.com>
2019-01-19 10:16:31 -0200

    check if at server

--- server/aliases.sh
+++ server/aliases.sh
@@ -1,3 +1,6 @@
 alias nginx-restart="sudo service nginx restart"
 alias addssl="sudo certbot --nginx"
 alias code="rmate"
+alias ATSERVER='echo "Welcome to ALIA server.. thread carefully"'
+
+$ATSERVER
