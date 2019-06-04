f37b93c078003d1ec446562342f7d64d94ad2259
Christhopher Lion <christhopherleao@icloud.com>
2019-06-04 13:40:23 +0100

    typo

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,16 @@
+nodeUpgrade() {
+  local nodeVersion=$(node -v)
+  nvm install node --latest-npm --reinstall-packages-from=$nodeVersion
+  nvm alias default node
+  nvm uninstall $nodeVersion
+  nvm cache clear
+}
+
+cloneSub() {
+  git clone --recursive $1
+  git submodule foreach --recursive "git checkout master"
+}
+
 if [[ "`source /etc/os-release && echo "$NAME"`" == "Ubuntu" ]]; then
   alias update="sudo apt update && sudo apt-get full-upgrade -y && sudo apt autoremove -y && npm cache verify && npm -g upgrade && npm cache verify"
   alias docker-on="sudo systemctl start docker"
@@ -29,8 +42,6 @@ alias zshrc="code ~/.zshrc"
 alias alia="ssh -R 52698:localhost:52698 api.alia.ml"
 
 # Docker utils
-alias docker-start="sudo systemctl start docker"
-alias docker-stop="sudo systemctl stop docker"
 alias stop="docker-compose kill"
 alias restart="docker-compose restart"
 alias containers="ctop -s mem"
@@ -38,17 +49,6 @@ alias log="docker-compose logs -f --tail 7"
 alias logtail="docker-compose logs -f --tail 100"
 alias build="sudo docker-compose build --no-cache --pull"
 alias up="docker-compose up -d"
+alias docker-stop="sudo systemctl stop docker"
+alias docker-start="sudo systemctl start docker"
 alias update-sub="git submodule update --init --recursive"
-
-nodeUpgrade() {
-  local nodeVersion=$(node -v)
-  nvm install node --latest-npm --reinstall-packages-from=$nodeVersion
-  nvm alias default node
-  nvm uninstall $nodeVersion
-  nvm cache clear
-}
-
-clone-sub() {
-  git clone --recursive $1
-  git submodule foreach --recursive "git checkout master"
-}
