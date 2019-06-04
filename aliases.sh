8782196d536cd681139ee07321a154eeab30fa84
Christhopher Lion <christhopherleao@icloud.com>
2019-06-04 14:07:05 +0100

    typo

--- aliases.sh
+++ aliases.sh
@@ -11,19 +11,19 @@ cloneSub() {
   git submodule foreach --recursive "git checkout master"
 }
 
-if [[ source /etc/os-release && echo $NAME == "Ubuntu" ]]; then
+if [[ "$(source /etc/os-release && echo $NAME)" == "Ubuntu" ]]; then
   alias update="sudo apt update && sudo apt-get full-upgrade -y && sudo apt autoremove -y && npm cache verify && npm -g upgrade && npm cache verify"
-elif [[ source /etc/os-release && echo $NAME == "Arch"* ]]; then
+elif [[ "$(source /etc/os-release && echo $NAME)" == "Arch"* ]]; then
   alias update="sudo pacman -Syu"
-elif [[ uname == "Darwin"* ]]; then
+elif [[ "`uname`" == "Darwin"* ]]; then
   alias update="brew update && brew upgrade && brew cask upgrade && brew cleanup && npm cache verify && npm -g upgrade && npm cache verify"
   alias sketch="sudo date 0314223218 && open /Applications/Sketch.app && sudo sntp -sS time.euro.apple.com"
 fi
 
-if [[ uname == "Linux" ]]; then
-  alias docker-on="sudo systemctl start docker"
-  alias docker-off="sudo systemctl stop docker"
-fi
+# if [[ "`uname`" == "Linux" ]]; then
+#   alias docker-on="sudo systemctl start docker"
+#   alias docker-off="sudo systemctl stop docker"
+# fi
 
 if grep -q Microsoft /proc/version; then
   alias docker="docker.exe"
@@ -46,6 +46,9 @@ alias log="docker-compose logs -f --tail 7"
 alias logtail="docker-compose logs -f --tail 100"
 alias build="sudo docker-compose build --no-cache --pull"
 alias up="docker-compose up -d"
-alias docker-stop="sudo systemctl stop docker"
 alias docker-start="sudo systemctl start docker"
+alias docker-on="sudo systemctl start docker"
+alias docker-stop="sudo systemctl stop docker"
+alias docker-off="sudo systemctl stop docker"
+
 alias update-sub="git submodule update --init --recursive"
