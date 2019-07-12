8d63cbe205f7b5692bceeb02d430d388da0c9b6d
christhopher.lion <christhopherleao@icloud.com>
2019-07-12 23:12:49 +0100

    update windows package installs

--- setup/windows.ps1
+++ setup/windows.ps1
@@ -2,10 +2,9 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 refreshenv
 choco install -y chocolatey-core.extension
 choco install -y nodejs
-choco install -y docker-desktop
+choco install -y docker-desktop --pre
 choco install -y googlechrome
 choco install -y googlechrome.canary
-choco install -y powershell-core
 choco install -y powershell-preview
 choco install -y hyper
 choco install -y git
@@ -13,13 +12,14 @@ choco install -y openssh
 choco install -y 7zip
 choco install -y curl
 choco install -y wget
+choco install -y gpg4win
 choco install -y yarn
 choco install -y vscode
 choco install -y vscode-insiders
 choco install -y k-litecodecpackfull
 choco install -y vlc
 choco install -y spotify
-choco install -y googledrive
+choco install google-backup-and-sync
 choco install -y gitkraken
 choco install -y insomnia-rest-api-client
 choco install -y firacode
