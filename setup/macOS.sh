c6ad45b14878b631ae3a81b5cc80d0008814326c
Christhopher Lion <christhopherleao@icloud.com>
2019-08-22 01:23:45 +0100

    mac key speed

--- setup/macOS.sh
+++ setup/macOS.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+
 # xcode command line - Select: "Get xcode" and go get a coffee (preferably far from your desk :)
 xcode-select --install
 
