b2dd552d643feb58961b1911bbbe82ac442c42d2
Christhopher Lion <christhopherleao@icloud.com>
2019-12-17 17:07:09 +0000

    story now uses jsx extenstion

--- scripts/newcomp.js
+++ scripts/newcomp.js
@@ -9,7 +9,7 @@ const templates = {
   component: fs.readFileSync(path.join(templatesFolder, 'component.jsx'), 'utf8'),
   styled: fs.readFileSync(path.join(templatesFolder, 'styled.js'), 'utf8'),
   test: fs.readFileSync(path.join(templatesFolder, 'test.js'), 'utf8'),
-  story: fs.readFileSync(path.join(templatesFolder, 'story.js'), 'utf8'),
+  story: fs.readFileSync(path.join(templatesFolder, 'story.jsx'), 'utf8'),
 };
 
 const args = process.argv.slice(2);
@@ -31,9 +31,7 @@ const copy = (file, ext) => {
   copy(templates.component, '.jsx');
   copy(templates.styled, '.styled.js');
   copy(templates.test, '.test.js');
-  copy(templates.story, '.story.js');
+  copy(templates.story, '.story.jsx');
 })()
 
 process.exit(0);
-
-
