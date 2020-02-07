972f8bd94c70fca5926e8965cd4d3c007ff20dab
Christhopher Lion <christhopherleao@icloud.com>
2020-02-07 18:01:17 +0000

    tweak linkshell

--- scripts/linkshell.sh
+++ scripts/linkshell.sh
@@ -1,8 +1,6 @@
-#!/usr/bin/env bash
-
 echo "Overriding your ZSHRC"
 
 LION_SHELL="$(npm root -g)/lion-shellscripts"
 
-echo "LION_SHELL='$LION_SHELL'" > $HOME/.zshrc
+echo "export LION_SHELL='$LION_SHELL'" > $HOME/.zshrc
 echo 'source $LION_SHELL/zshconfig.sh' >> $HOME/.zshrc
