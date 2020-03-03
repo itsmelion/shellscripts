c6e9d9ef2d58c67ad70c4ff8015d43daf634eca6
Christhopher Leão <christhopherleao@icloud.com>
2020-03-03 18:41:32 +0000

    removed javahome

--- zshconfig.sh
+++ zshconfig.sh
@@ -68,7 +68,6 @@ export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
-export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 
 # Ruby
 export PATH="/usr/local/opt/ruby/bin:$PATH"
