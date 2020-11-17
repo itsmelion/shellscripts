265f1b4ff2df908969a883a5d466f919a04314aa
Christhopher Leão <christhopherleao@icloud.com>
2020-11-17 17:35:38 +0000

    override shell shows message

--- scripts/linkshell.sh
+++ scripts/linkshell.sh
@@ -1,6 +1,6 @@
-echo "Overriding your ZSHRC"
-
 LION_SHELL="$(npm root -g)/lion-shellscripts"
+echo "Overriding your ZSHRC to $LION_SHELL"
+echo "dont forget your secrets."
 
 echo "export LION_SHELL='$LION_SHELL'" > $HOME/.zshrc
 echo 'source $LION_SHELL/zshconfig.sh' >> $HOME/.zshrc
