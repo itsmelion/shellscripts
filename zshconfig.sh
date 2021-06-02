ead1dee98315b07bda9d4c5e1b9ed4ba0d152f5e
Christhopher Lion <itsmelion@icloud.com>
2021-06-02 16:48:41 +0200

    remove nodeoptions hack

--- zshconfig.sh
+++ zshconfig.sh
@@ -42,8 +42,6 @@ export LANGUAGE="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 export GPG_TTY=$(tty)
 
-# ! this is weird
-export NODE_OPTIONS='--max-old-space-size=4096'
 
 # Preferred editor for local and remote sessions
 if [[ -n $SSH_CONNECTION ]]; then
