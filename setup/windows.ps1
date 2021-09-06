6fd1c05790b862e8b48e520d85fb26b1789c0d38
Christhopher Lion <christhopherleao@icloud.com>
2021-09-06 11:40:46 +0200

    windows add invert scroll direction
    Signed-off-by: Christhopher Lion <christhopherleao@icloud.com>

--- setup/windows.ps1
+++ setup/windows.ps1
@@ -1,5 +1,10 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# invert mouse scroll direction
+Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Enum\HID\*\*\Device` Parameters FlipFlopWheel -EA 0 | ForEach-Object { Set-ItemProperty $_.PSPath FlipFlopWheel 1 }
+
 refreshenv
+
 choco feature enable -n allowGlobalConfirmation
 choco install -y chocolatey-core.extension
 choco install -y nodejs
@@ -26,10 +31,11 @@ choco install -y thunderbird
 choco install -y icloud
 
 refreshenv
+
 cp ..\git\.gitconfig $HOME
 git config --global user.name "Christhopher Lion"
 git config --global user.email christhopherleao@icloud.com
-git config --global core.editor code
+git config --global core.editor "code-insiders"
 cp ..\git\.gitignore $HOME
 git config --global core.excludesfile $HOME\.gitignore
 
