3588ad00aed3e9e605c3acc42a65047ae00b3bd6
Christhopher Lion <christhopherleao@icloud.com>
2021-12-26 13:33:30 -0300

    update nvm script
    Signed-off-by: Christhopher Lion <christhopherleao@icloud.com>

--- setup/common.sh
+++ setup/common.sh
@@ -60,7 +60,7 @@ sshSetup
 echo "\n\n»  Installing nice apps..\n"
 
 # Node NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 xdg-mime default code.desktop text/plain
 
