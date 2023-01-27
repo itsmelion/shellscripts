600ce067c755ade99cf671a50bb07db8409d4e8c
Christhopher Lion <christhopherleao@icloud.com>
2023-01-27 11:28:52 +0100

    google cloud
    Signed-off-by: Christhopher Lion <christhopherleao@icloud.com>

--- zshconfig.sh
+++ zshconfig.sh
@@ -108,4 +108,12 @@ add-zsh-hook chpwd load-nvmrc
 load-nvmrc
 # End: Calling nvm use automatically
 
+# source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc' ]; then . '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc' ]; then . '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'; fi
+
+
 plugins=(zsh-syntax-highlighting)
